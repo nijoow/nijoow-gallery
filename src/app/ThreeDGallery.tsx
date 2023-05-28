5190d005a74d76f64074c0fd64dcdbfe289d511d
Woojin Lee <nijoow1127@gmail.com>
2023-05-28 22:46:33 +0900

    🎨 orthographic 옵션 추가

--- src/app/ThreeDGallery.tsx
+++ src/app/ThreeDGallery.tsx
@@ -5,8 +5,20 @@ import * as THREE from "three";
 
 const ThreeDGallery = () => {
   return (
-    <div className="w-screen h-screen">
-      <Canvas>
+    <div className="w-screen h-screen bg-violet-200">
+      <Canvas
+        camera={{
+          left: -window.innerWidth / 2,
+          right: window.innerWidth / 2,
+          top: window.innerHeight / 2,
+          bottom: -window.innerHeight / 2,
+          near: 0.1,
+          far: 1000,
+          position: [10, 10, 10],
+          zoom: 3,
+        }}
+        orthographic
+      >
         <ambientLight />
         <pointLight position={[10, 10, 10]} />
       </Canvas>
